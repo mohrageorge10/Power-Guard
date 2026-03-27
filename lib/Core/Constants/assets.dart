013f9f492924218058c8c240e12f260d1adb4213
Mohraeel George <mohraeelgeorge10@gmail.com>
2026-03-27 19:59:31 +0200

    Replace entire project files with new ones

---
+++ lib/Core/Constants/assets.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  /// Assets for imagesBolt
+  /// assets/images/bolt.png
+  static const String imagesBolt = "assets/images/bolt.png";
+
+  /// Assets for imagesClock
+  /// assets/images/clock.png
+  static const String imagesClock = "assets/images/clock.png";
+
+  /// Assets for imagesDepLead
+  /// assets/images/dep_lead.png
+  static const String imagesDepLead = "assets/images/dep_lead.png";
+
+  /// Assets for imagesManager
+  /// assets/images/manager.png
+  static const String imagesManager = "assets/images/manager.png";
+
+  /// Assets for imagesMoneyBag
+  /// assets/images/money_bag.png
+  static const String imagesMoneyBag = "assets/images/money_bag.png";
+
+  /// Assets for imagesOwner
+  /// assets/images/owner.png
+  static const String imagesOwner = "assets/images/owner.png";
+
+  /// Assets for imagesPredict
+  /// assets/images/predict.png
+  static const String imagesPredict = "assets/images/predict.png";
+}
+
